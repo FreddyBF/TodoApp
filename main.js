70895047d1fbeed8d50b86d4ee4a7e445d119008
freddy <baptistaalfredo81@gmail.com>
2024-11-06 15:02:37 +0100

    feat: interactividade com js

--- main.js
+++ main.js
@@ -1,2 +1,49 @@
 
+const changeTheme = document.querySelector('.head__logo img');
+
+const html = document.documentElement;
+
+const list = document.querySelector('.main__list');
+
+const input = document.querySelector(".head__input input");
+
+
+function createListItem(task) {
+    return `
+        <li class="main__list-item">
+				<div class="main__circle"></div>
+				<span class="main__task">${task}</span>
+				<div class="main__logo">
+					<img src="./images/icon-cross.svg" alt="eliminar">
+				</div>
+		</li>
+    `
+}
+
+input.addEventListener("keydown", (event)=>{
+    if(event.key === "Enter") {
+        const task = input.value;
+        input.value = '';
+        const li = createListItem(task);
+        const ul = document.querySelector('.main__list');
+        ul.insertAdjacentHTML('afterbegin', li);
+    }
+});
+
+changeTheme.addEventListener('click', ()=>{
+
+    const src = changeTheme.getAttribute("class");
+
+    if(src === 'head__logo--moon') {
+        changeTheme.setAttribute("src", './images/icon-sun.svg');
+        changeTheme.classList.toggle("head__logo--moon");
+        html.dataset.theme = 'dark';
+    }
+    else {
+        changeTheme.setAttribute("src", './images/icon-moon.svg');
+        changeTheme.classList.toggle("head__logo--moon");
+        html.dataset.theme = 'light';
+    }
+});
+
 
